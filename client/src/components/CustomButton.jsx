fa2083898158da62aa399277e660f49505e259c1
Artemonaaa <artemidaabc@gmail.com>
2023-07-24 22:58:59 +0300

    add component customButton and add page Home

---
+++ client/src/components/CustomButton.jsx
@@ -0,0 +1,29 @@
+/* eslint-disable react/prop-types */
+import { useSnapshot } from 'valtio'
+
+import state from '../store'
+
+const CustomButton = ({ type, title, customStyles, handleClick }) => {
+  const snape = useSnapshot(state)
+
+  const generalStyle = (type) => {
+    if(type === "filled") {
+      return {
+        backgroundColor: snape.color,
+        color: "#fff"
+      }
+    }
+  }
+
+  return (
+    <button
+      className={`px-2 py-1.5 flex-1 rounded-md ${customStyles}`}
+      style={generalStyle(type)}
+      onClick={handleClick }
+    >
+      {title}
+    </button>
+  )
+}
+
+export default CustomButton
