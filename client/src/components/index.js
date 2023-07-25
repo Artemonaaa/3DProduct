6ebef5c144db38edce1780c225b58f4bc3c838b6
Artemonaaa <artemidaabc@gmail.com>
2023-07-25 21:56:35 +0300

    add some components

---
+++ client/src/components/index.js
@@ -0,0 +1,13 @@
+import CustomButton from "./CustomButton"
+import AIPicker from "./AIPicker"
+import ColorPicker from "./ColorPicker"
+import FilePicke from "./FilePicke"
+import Tab from "./Tab"
+
+export {
+  CustomButton,
+  AIPicker,
+  ColorPicker,
+  FilePicke,
+  Tab
+}
